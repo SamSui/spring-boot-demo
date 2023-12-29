385d733a21df34cc6ebfb8e165c7892715204647
suixiangxi <suixiaox@163.com>
2023-12-29 11:03:09 +0800

    add db app

--- spring-boot-db/src/main/java/com/xisui/springbootdb/SpringBootDbApplication.java
+++ spring-boot-db/src/main/java/com/xisui/springbootdb/SpringBootDbApplication.java
@@ -3,7 +3,7 @@ package com.xisui.springbootdb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.xisui.springbootdb")
 public class SpringBootDbApplication {
 
     public static void main(String[] args) {
