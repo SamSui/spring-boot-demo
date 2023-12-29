c4c329f50c7bab7852a55fc34058677ed7b9963b
suixiangxi <suixiaox@163.com>
2023-12-29 10:55:18 +0800

    add db app

---
+++ spring-boot-db/src/test/java/com/xisui/springbootdb/SpringBootDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xisui.springbootdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
