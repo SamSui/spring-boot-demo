84c4fedf4bb2409a408b6542afd961e0082f77f2
suixiangxi <suixiaox@163.com>
2024-03-08 15:19:45 +0800

    ai demo

--- spring-boot-ai/src/main/java/com/xisui/springbootai/OpenAIApiTest.java
+++ spring-boot-ai/src/main/java/com/xisui/springbootai/OpenAIApiTest.java
@@ -2,6 +2,8 @@ package com.xisui.springbootai;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -13,26 +15,31 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 
 public class OpenAIApiTest {
+    public static CloseableHttpClient httpClient;
+    static {
+        HttpHost proxy = new HttpHost("127.0.0.1", 10809, "http");
+        //把代理设置到请求配置
+        RequestConfig defaultRequestConfig = RequestConfig.custom().setProxy(proxy).build();
+        httpClient = HttpClients.custom().setDefaultRequestConfig(defaultRequestConfig).build();
+    }
     public static void main(String[] args) {
-        testGet();
+        //testGet();
+        testPost();
     }
 
     public static void testGet(){
         // 设置 OpenAI API 的密钥
-        String apiKey = "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";
+        String apiKey = "ddd";
 
         // 设置请求 URL
         String apiUrl = "https://api.openai.com/v1/models";
 
-        // 创建 HttpClient 对象
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+        try {
             // 创建 HttpGet 请求对象
             HttpGet httpGet = new HttpGet(apiUrl);
-
             // 设置请求头
             httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
             httpGet.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey);
-
 
             // 发送请求并获取响应
             try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
@@ -49,16 +56,16 @@ public class OpenAIApiTest {
     }
     public static void testPost(){
         // 设置 OpenAI API 的密钥
-        String apiKey = "xxxx";
+        String apiKey = "ddd";
 
         // 设置请求 URL
         String apiUrl = "https://api.openai.com/v1/completions";
 
         // 设置请求的 JSON 数据
-        String requestData = "{\"prompt\": \"Once upon a time\", \"max_tokens\": 50}";
+        String requestData = "{\"prompt\": \"推荐一部电影\", \"max_tokens\": 2048, \"model\": \"gpt-3.5-turbo-0125\"}";
 
         // 创建 HttpClient 对象
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+        try{
             // 创建 HttpPost 请求对象
             HttpPost httpPost = new HttpPost(apiUrl);
 
